05e4febcd7800e7343435e430403b58cc1b8c183
EvgeniJD <evgeni.y.dimitrov1985@gmail.com>
2021-05-30 14:42:11 +0300

    try custom useFetch hook

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,18 +1,21 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+import useFetch from '../../hooks/useFetch';
 import Post from './Post';
 import postsService from '../../services/posts';
 
 function Posts() {
 
-    const [ posts, setPosts ] = useState([]);
+    // const [ posts, setPosts ] = useState([]);
 
-    useEffect(() => {
-        postsService.getAll()
-            .then(posts => {
-                setPosts(posts)
-            })
-            .catch(e => console.log("Posts Error Handler: ", e.message));
-    }, [])
+    const [ posts ] = useFetch(postsService.getAll, []);
+
+    // useEffect(() => {
+    //     postsService.getAll()
+    //         .then(posts => {
+    //             setPosts(posts)
+    //         })
+    //         .catch(e => console.log("Posts Error Handler: ", e.message));
+    // }, [])
 
     return (
         posts.map(post =>
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+
+function useFetch(fetchFunction, initialValue) {
+    const [state, setState] = useState(initialValue);
+
+    useEffect(() => {
+        fetchFunction()
+            .then(result => {
+                console.log("Result: ", result);
+                setState(result);
+            })
+            .catch(e => console.log("Error from useFetch: ", e.message));
+    }, [fetchFunction])
+
+    return [
+        state
+    ]
+    // useEffect(() => {
+    //     fetch(`${baseURL}/posts`, {
+    //         credentials: "include",
+    //     })
+    //         .then(res => {
+    //             if (!res.ok) {
+    //                 throw new Error('Can not get posts!');
+    //             }
+    //             return res.json()
+    //         })
+    //         .catch(e => console.log("Error from postsService: ", e.message));
+    // }, [])
+}
+
+export default useFetch;
